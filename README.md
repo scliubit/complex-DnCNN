3b59d24cd5c5dfe1b7895285d45a6343be5887c0
psycholsc <13513034769@163.com>
2020-08-14 01:03:12 +0800

    init
    main code (part I)

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# complex-DnCNN
+## complex-valued DnCNN
+
+Source code for [Deep Denoising Neural Network Assisted Compressive Channel Estimation for mmWave Intelligent Reflecting Surfaces](https://ieeexplore.ieee.org/document/9127834), modified from [DnCNN](https://github.com/cszn/DnCNN). 
+
+
+
+To successfully run the code, you should follow the steps below
+
+- Make sure you have installed the required libs
+
+- Generate channel dataset. We have used the geometric channel model in our paper with vary parameters. 
+
+Note that the required shape is $[N,2,N_{IRS},N_{C}]$, where 2 represents the real part and imaginary part of the channel matrix.
+
+- Set the parameters in `train_cDnCNN.py` and create the corresponding folders before running the program.
+- Run `test_cDnCNN.py` to evaluate the performance.
+
+---
+
+文章 [Deep Denoising Neural Network Assisted Compressive Channel Estimation for mmWave Intelligent Reflecting Surfaces](https://ieeexplore.ieee.org/document/9127834) 的源代码, 由 [DnCNN](https://github.com/cszn/DnCNN) 等修改。
+
+为保证成功运行，您应按照以下步骤进行
+
+- 确保您已安装所有需要的库
+- 产生一组信道数据集。此处我们采用几何信道建模，参数在仿真中有变。
+
+值得注意的是，信道数据集的尺寸要求为$[N,2,N_{IRS},N_{C}]$，其中2代表信道矩阵的实部和虚部。
+
+- 运行`train_cDnCNN.py`进行训练，在这之前要设置合适的参数（用于区别保存模型时的路径）。
+- 运行`test_cDnCNN.py`测试性能表现。
+
+---
+
+> 原文采用OMP算法构造数据集，该部分代码可能在后续整理中发布。
---
+++ utils.py
@@ -0,0 +1,45 @@
+import torch
+import numpy as np
+
+
+def cov(m, rowvar=False):
+    '''Estimate a covariance matrix given data.
+
+    Covariance indicates the level to which two variables vary together.
+    If we examine N-dimensional samples, `X = [x_1, x_2, ... x_N]^T`,
+    then the covariance matrix element `C_{ij}` is the covariance of
+    `x_i` and `x_j`. The element `C_{ii}` is the variance of `x_i`.
+
+    Args:
+        m: A 1-D or 2-D array containing multiple variables and observations.
+            Each row of `m` represents a variable, and each column a single
+            observation of all those variables.
+        rowvar: If `rowvar` is True, then each row represents a
+            variable, with observations in the columns. Otherwise, the
+            relationship is transposed: each column represents a variable,
+            while the rows contain observations.
+
+    Returns:
+        The covariance matrix of the variables.
+    '''
+    if m.dim() > 2:
+        raise ValueError('m has more than 2 dimensions')
+    if m.dim() < 2:
+        m = m.view(1, -1)
+    if not rowvar and m.size(0) != 1:
+        m = m.t()
+    # m = m.type(torch.double)  # uncomment this line if desired
+    fact = 1.0 / (m.size(1) - 1)
+    m -= torch.mean(m, dim=1, keepdim=True)
+    mt = m.t()  # if complex: mt = m.t().conj()
+    return fact * m.matmul(mt).squeeze()
+
+
+if __name__ == "__main__":
+    r = False
+    x = np.random.randn(30, 2)
+    xt = torch.from_numpy(x).type(torch.double)
+    np_c = np.cov(x, rowvar=r)
+    our_c = cov(xt, rowvar=r).numpy()
+    print(np.allclose(np_c, our_c))
+    print(x, '\n\n\n', our_c, '\n\n\n', torch.var(xt))
